1bc0f50e5bf4066241831e9c69ee593ec74c81a6
kchristin <christinakoutsou22@gmail.com>
2024-07-17 10:13:08 -0500

    check mask of vxm separately

--- experimental/algorithm/LAGraph_MaximumMatching.c
+++ experimental/algorithm/LAGraph_MaximumMatching.c
@@ -690,28 +690,32 @@ int LAGraph_MaximumMatching(
                 {
                     // the frontierC vector is sparse or hypersparse
                     // push (vector's values are pushed to A)
-                    GRB_TRY(GrB_vxm(frontierR, parentsR, NULL,
+                    GRB_TRY(GrB_Vector_clear(frontierR));
+                    GRB_TRY(GrB_vxm(frontierR, NULL, NULL,
                                     MinParent_1st_Semiring, frontierC, AT,
-                                    GrB_DESC_RSC));
+                                    NULL));
+                    GRB_TRY(GrB_Vector_assign(frontierR, parentsR, NULL,
+                                              frontierR, GrB_ALL, nrows,
+                                              GrB_DESC_RSC));
                 }
             }
-            else
-            {
-                if (A != NULL)
-                {
-                    // Only the pull method can be used if AT is not given
-                    GRB_TRY(GrB_mxv(frontierR, parentsR, NULL,
-                                    MinParent_2nd_Semiring, A, frontierC,
-                                    GrB_DESC_RSC));
-                }
-                else
-                {
-                    // Only the push method can be used if A is not given
-                    GRB_TRY(GrB_vxm(frontierR, parentsR, NULL,
-                                    MinParent_1st_Semiring, frontierC, AT,
-                                    GrB_DESC_RSC));
-                }
-            }
+            // else
+            // {
+            //     if (A != NULL)
+            //     {
+            //         // Only the pull method can be used if AT is not given
+            //         GRB_TRY(GrB_mxv(frontierR, parentsR, NULL,
+            //                         MinParent_2nd_Semiring, A, frontierC,
+            //                         GrB_DESC_RSC));
+            //     }
+            //     else
+            //     {
+            //         // Only the push method can be used if A is not given
+            //         GRB_TRY(GrB_vxm(frontierR, parentsR, NULL,
+            //                         MinParent_1st_Semiring, frontierC, AT,
+            //                         GrB_DESC_RSC));
+            //     }
+            // }
             t = LAGraph_WallClockTime() - t;
             mxm_time += t;
 
