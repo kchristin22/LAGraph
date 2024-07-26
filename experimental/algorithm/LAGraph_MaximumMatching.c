de87add96ef334c019dcf9eca48b3898876ee600
kchristin <christinakoutsou22@gmail.com>
2024-07-26 18:20:21 -0500

    Check if frontierR is equal using either vxm or mxv with the bug

--- experimental/algorithm/LAGraph_MaximumMatching.c
+++ experimental/algorithm/LAGraph_MaximumMatching.c
@@ -674,18 +674,18 @@ int LAGraph_MaximumMatching(
                 {
                     // the frontierC vector is sparse or hypersparse
                     // push (vector's values are pushed to A)
-                    GRB_TRY(GrB_vxm(frontierR, NULL, NULL,
-                                    MinParent_1st_Semiring, frontierC, AT,
-                                    GrB_DESC_R));
-                    // printf("vxm\n");
-                    GRB_TRY(GrB_Vector_assign(frontierR, parentsR, NULL,
-                                              frontierR, GrB_ALL, nrows,
-                                              GrB_DESC_RSC));
+                    // GRB_TRY(GrB_vxm(frontierR, NULL, NULL,
+                    //                 MinParent_1st_Semiring, frontierC, AT,
+                    //                 GrB_DESC_R));
+                    // // printf("vxm\n");
+                    // GRB_TRY(GrB_Vector_assign(frontierR, parentsR, NULL,
+                    //                           frontierR, GrB_ALL, nrows,
+                    //                           GrB_DESC_RSC));
 
                     // version that produces a segmentation fault:
-                    // GRB_TRY(GrB_vxm(frontierR, parentsR, NULL,
-                    //                 MinParent_1st_Semiring, frontierC, AT,
-                    //                 GrB_DESC_RSC));
+                    GRB_TRY(GrB_vxm(frontierR, parentsR, NULL,
+                                    MinParent_1st_Semiring, frontierC, AT,
+                                    GrB_DESC_RSC));
 
                     GrB_Vector frontierR_dup = NULL;
                     GRB_TRY(GrB_Vector_new(&frontierR_dup, Vertex, nrows));
